0a05d8a3c051f741ab090abf196370a80bbdcbf3
Denis <trinitiwowka@gmail.com>
2016-12-10 19:20:42 +0200

    work add new item cart

--- js/view_cart.js
+++ js/view_cart.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
+    var productArray = [];
 
+    Storage.prototype.getObj = function (key) {
+        var value = this.getItem(key);
+        return value && JSON.parse(value);
+    };
+
+
+    if (localStorage.getObj('product') !== null) {
+        productArray = localStorage.getObj('product');
+        for(var i=0;i<productArray.length;i++)
+        {
+            var tmp="<tr><td>"+productArray[i]["name"]+"</td>";
+            tmp+="<td>"+"<input type=\"text\" class=\"input_count\"/>"+"</td>";
+            tmp+="<td>"+productArray[i]["price"]+"</td>";
+            tmp+="<td>"+productArray[i]["size"]+"</td>";
+            tmp+="<td>"+productArray[i]["color"]+"</td></tr>";
+            $(".table").append(tmp);
+        }
+    }
+    else {
+        console.log("storage empty");
+    }
 });
